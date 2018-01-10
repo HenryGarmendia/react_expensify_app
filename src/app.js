8d7a4b24af9e48de770bbb8983079e54073ba54a
HenryGarmendia <me@henrygarmendia.com>
2018-01-09 22:13:11 -0600

    modified package.json, app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionWebApp from './components/indecision_web_app';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
