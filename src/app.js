336f4ee62da8e4c6b5a141709757b13ff364e615
HenryGarmendia <me@henrygarmendia.com>
2018-01-09 22:50:15 -0600

    import BrowserRouter, Route to project

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+
+const routes = (
+    
+);
 
 ReactDOM.render(<p>Rendering Boilerplate</p>, document.getElementById('react_container'));
