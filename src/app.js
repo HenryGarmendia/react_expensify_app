02092c5c1548f86dcda43fd256f198a5bbc7cb75
HenryGarmendia <me@henrygarmendia.com>
2018-01-10 08:59:42 -0600

    Implement switch to the project to handle route render

--- src/app.js
+++ src/app.js
@@ -1,41 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const ExpenseDashboard = () => (
     <div>
-        This is from my DASHBOARD component
+        This is from DASHBOARD component...
     </div>
 );
 
 const AddExpense = () => (
     <div>
-        This is from my ADD component
+        This is from ADD component...
     </div>
 );
 
 const EditExpense = () => (
     <div>
-        This is from my EDIT component
+        This is from EDIT component...
     </div>
 );
 
 const HelpPage = () => (
     <div>
-        This is from my HELP component
+        This is from HELP component...
+    </div>
+);
+
+const NotFoundPage = () => (
+    <div>
+        ⚠️ 404 Page Not Found...
     </div>
 );
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={ExpenseDashboard} exact={true}/>
             <Route path="/create" component={AddExpense} />
             <Route path="/edit" component={EditExpense} />
             <Route path="/help" component={HelpPage} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
