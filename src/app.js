a6eb61090937aa317a31416af72368265b2c0bfa
HenryGarmendia <me@henrygarmendia.com>
2018-01-09 22:01:49 -0600

    First commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionWebApp from './components/indecision_web_app';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>Rendering Boilerplate</p>, document.getElementById('react_container'));
