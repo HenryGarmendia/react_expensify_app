21400d646154c62471ddd277edf1fec1d506ca48
HenryGarmendia <me@henrygarmendia.com>
2018-01-11 23:30:50 -0600

    addes some ES6 distructuring example to file

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,1 +1,14 @@
-console.log('destructuring');
+const person = {
+    name: 'Henry',
+    age: 37,
+    location: {
+        city: 'San Francisco',
+        temp: 55
+    }
+};
+
+const {name, age} = person;
+const {city, temp} = person.location;
+
+console.log(`${name} is ${age} and he is from ${city}.`);
+console.log(`is ${temp} degrees in ${city}.`);
