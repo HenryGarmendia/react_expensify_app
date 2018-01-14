48e9fdc10f38b6f71d5d372f11040975546114da
HenryGarmendia <me@henrygarmendia.com>
2018-01-14 00:07:30 -0600

    finish the dummy data for redux_expensify.js

--- src/playground/redux_expensify.js
+++ src/playground/redux_expensify.js
@@ -6,6 +6,12 @@ const demoState = {
         description: 'JavaScript Course',
         note: 'Et excepteur quis elit proident veniam laboris excepteur labore.',
         amount: 1099,
-        createAt:
-    }]
+        createAt: 0
+    }],
+    filters: {
+        text: 'course',
+        sortBy: 'amount', // date or amount
+        startDate: undefined,
+        endDate: undefined
+    }
 }
