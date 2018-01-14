3be123c4d9faf1c08e082a174b1cb3205c662795
HenryGarmendia <me@henrygarmendia.com>
2018-01-14 00:02:28 -0600

    testing createStore and combineReducer from redux

---
+++ src/playground/redux_expensify.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux';
+
+const demoState = {
+    expenses: [{
+        id: 'HenryGarmendia',
+        description: 'JavaScript Course',
+        note: 'Et excepteur quis elit proident veniam laboris excepteur labore.',
+        amount: 1099,
+        createAt:
+    }]
+}
