9393ac827ec7af6cc81c636558706600dcebc817
HenryGarmendia <me@henrygarmendia.com>
2018-01-15 13:24:46 -0600

    addes new files and folder to organize redux

--- src/playground/redux_expensify.js
+++ src/playground/redux_expensify.js
@@ -131,6 +131,23 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
     }
 };
 
+//Get visible expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createAt < b.createAt ? 1 : -1;
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
 //Store Creation
 const store = createStore(
     combineReducers({
@@ -140,23 +157,23 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
 //Store Dispatch Calls
-const expenseOne = store.dispatch(addExpense({ description: 'MacBook Pro', amount: 300000 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+const expenseOne = store.dispatch(addExpense({ description: 'MacBook Pro', amount: 300000, createAt: -21000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createAt: -1000 }));
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }));
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
-store.dispatch(setTextFilter('course'));
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+// store.dispatch(setTextFilter('course'));
 store.dispatch(sortByAmount());
-store.dispatch(sortByDate());
-store.dispatch(setStartDate(125));
-store.dispatch(setStartDate());
-store.dispatch(setEndDate(1250));
-
-
+// store.dispatch(sortByDate());
+// store.dispatch(setStartDate(125));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(1250));
 
 const demoState = {
     expenses: [{
