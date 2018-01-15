4048e42e795a6fdb3c7626b6ed34ba38c9612704
HenryGarmendia <me@henrygarmendia.com>
2018-01-15 08:02:30 -0600

    created 2 more actions generator for startDate, endDate

--- src/playground/redux_expensify.js
+++ src/playground/redux_expensify.js
@@ -50,7 +50,16 @@ const sortByAmount = () => ({
 });
 
 //SET_START_DATE
+const setStartDate = (startDate) => ({
+    type: 'SET_START_DATE',
+    startDate
+});
+
 //SET_END_DATE
+const setEndDate = (endDate) => ({
+    type: 'SET_END_DATE',
+    endDate
+});
 
 //Expenses Reducer
 const expensesReducerDefaultState = [];
@@ -107,6 +116,16 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
                 ...state,
                 sortBy: 'amount'
             };
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                setStartDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                setStartDate: action.endDate
+            };
         default:
             return state;
     }
@@ -131,9 +150,13 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
 store.dispatch(setTextFilter('course'));
-
 store.dispatch(sortByAmount());
 store.dispatch(sortByDate());
+store.dispatch(setStartDate(125));
+store.dispatch(setStartDate());
+store.dispatch(setEndDate(1250));
+
+
 
 const demoState = {
     expenses: [{
