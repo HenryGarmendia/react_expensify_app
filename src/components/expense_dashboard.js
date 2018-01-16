3e5b934a66164111e17f24040befa7eedf64b330
HenryGarmendia <me@henrygarmendia.com>
2018-01-16 07:28:38 -0600

    imported expense_list_filter to expense_dashboard

--- src/components/expense_dashboard.js
+++ src/components/expense_dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './expense_list';
+import ExpenseListFilters from './expense_list_filters';
 
 const ExpenseDashboard = () => (
     <div>
         This is from DASHBOARD component...
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
