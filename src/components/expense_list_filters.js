fc100515c340f7c900e68c49fb7ded14b2e7838a
HenryGarmendia <me@henrygarmendia.com>
2018-01-16 08:02:46 -0600

    created a dispatch from components

--- src/components/expense_list_filters.js
+++ src/components/expense_list_filters.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { setTextFilter } from '../actions/filters';
 const ExpenseListFilters = (props) => (
     <div>
-        <input type="text" value={props.filters.text} />
-    </div>
+        <input type="text" value={props.filters.text} onChange={(e) => {
+            props.dispatch(setTextFilter(e.target.value));
+        }} />
+    </div>  
 );
 
 const mapStateToProps = (state) => {
