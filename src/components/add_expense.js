e0a0e2626bb910d93892b141ec6e7001eee221ea
HenryGarmendia <me@henrygarmendia.com>
2018-01-17 08:16:14 -0600

    dispatch the giving action to redux store

--- src/components/add_expense.js
+++ src/components/add_expense.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import ExpenseForm from './expense_form';
+import { addExpense } from '../actions/expenses';
 
-const AddExpense = () => (
+const AddExpense = (props) => (
     <div>
         This is from ADD component...
         <h1>Add Expense</h1>
         <ExpenseForm 
             onSubmit={(expense) => {
-                console.log(expense);
+                props.dispatch(addExpense(expense));
             }}
         />
     </div>
 );
 
-export default AddExpense;
+export default connect()(AddExpense);
