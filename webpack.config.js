114090c8630fabc252572db1e5e5ab4e4217e0e2
HenryGarmendia <me@henrygarmendia.com>
2018-01-18 10:43:41 -0600

    getting webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
-var path = require('path');
+const path = require('path');
 
-module.exports = { 
-	entry: './src/app.js',
-	output: { 
-		path: path.resolve(__dirname,  "public"),
-		filename: "bundle.js" 
-	},
-	module: { 
-		rules: [ 
-			{ 
-				loader: 'babel-loader',
-				test:  /\.js$/,
-				exclude: /node_modules/ 
-			},
-			{
-				test: /\.s?css$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					'sass-loader'
-				]
-			} 
-		] 
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.resolve(__dirname,  "public"),
-		historyApiFallback: true
-	}
+module.exports = (env) => {
+	const isProduction = env == 'production';
+
+	return { 
+		entry: './src/app.js',
+		output: { 
+			path: path.resolve(__dirname,  "public"),
+			filename: "bundle.js" 
+		},
+		module: { 
+			rules: [ 
+				{ 
+					loader: 'babel-loader',
+					test:  /\.js$/,
+					exclude: /node_modules/ 
+				},
+				{
+					test: /\.s?css$/,
+					use: [
+						'style-loader',
+						'css-loader',
+						'sass-loader'
+					]
+				} 
+			] 
+		},
+		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.resolve(__dirname,  "public"),
+			historyApiFallback: true
+		}
+	};
 };
