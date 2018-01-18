f71dcca7a6338c5e6b0c2e9c76d0819363ee4d7e
HenryGarmendia <me@henrygarmendia.com>
2018-01-18 16:13:37 -0600

    setup devDependencies and dist folder

--- src/app.js
+++ src/app.js
@@ -11,13 +11,6 @@ import AppRouter from './routers/app_router';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 45000}));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 3500, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500}));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
