094de90f079195dce6caab8f3975e1ebad2b5b56
HenryGarmendia <me@henrygarmendia.com>
2018-01-23 17:55:05 -0600

    created a snapshot of project with ReactShallowRenderer

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify Web-App</h1>
         <NavLink activeClassName="is_active link" exact={true} to="/">Dashboard</NavLink>
         <NavLink activeClassName="is_active link" to="/create">Create Expense</NavLink>
-        <NavLink activeClassName="is_active link" to="/help">Help</NavLink>
     </header>
 );
 
