cb76d4efa230c5b072a8679d6516ae8a411ae39d
HenryGarmendia <me@henrygarmendia.com>
2018-01-28 23:39:34 -0600

    modified the Header.test with enzyme

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/header';
 
 test('should render header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
-    console.log(renderer.getRenderOutput());
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('h1').text()).toBe('Expensify Web-App');
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+    // console.log(renderer.getRenderOutput());
 });
