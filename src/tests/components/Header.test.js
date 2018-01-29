327ddbbf1b42544b53faf909dbddc8f6c0deb11c
HenryGarmendia <me@henrygarmendia.com>
2018-01-29 17:35:37 -0600

    finalized the setup for enzymy

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -4,9 +4,5 @@ import Header from '../../components/header';
 
 test('should render header correctly', () => {
     const wrapper = shallow(<Header />);
-    expect(wrapper.find('h1').text()).toBe('Expensify Web-App');
-    // const renderer = new ReactShallowRenderer();
-    // renderer.render(<Header />);
-    // expect(renderer.getRenderOutput()).toMatchSnapshot();
-    // console.log(renderer.getRenderOutput());
+    expect(wrapper).toMatchSnapshot();
 });
