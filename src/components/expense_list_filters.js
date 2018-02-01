66fbcebb3bd368399f192dee49f7cb09a46c2f96
HenryGarmendia <me@henrygarmendia.com>
2018-01-31 20:42:22 -0600

    added test to NotfoundPage and ExpenseDashboardPage

--- src/components/expense_list_filters.js
+++ src/components/expense_list_filters.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { DateRangePicker } from 'react-dates';
+import { DateRangePicker } from 'react-dates/initialize';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters';
 
 class ExpenseListFilters extends React.Component {
