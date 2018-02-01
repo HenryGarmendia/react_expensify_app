45ca3ce3603d39928a609c5717e59dc5464e1ced
HenryGarmendia <me@henrygarmendia.com>
2018-01-31 19:58:12 -0600

    ran the test for the ExpenseListItem Snapshot

--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -2,3 +2,8 @@ import React from 'react';
 import { shallow } from "enzyme";
 import expenses from '../fixtures/expenses';
 import ExpenseListItem from '../../components/expense_list_item';
+
+test('should render ExpenseListItem correctly', () => {
+    const wrapper = shallow(<ExpenseListItem {...expenses[0]} />);
+    expect(wrapper).toMatchSnapshot();
+});
