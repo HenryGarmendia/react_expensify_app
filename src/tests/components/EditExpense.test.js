66b7984603bb6b1f95d5d6a956da6cc18c58790c
HenryGarmendia <me@henrygarmendia.com>
2018-02-17 12:39:54 -0600

    created and modified the EditExpense to handle test

---
+++ src/tests/components/EditExpense.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import { EditExpense } from '../../components/edit_expense';
+
+// should render EditExpense
+
+// should handle
