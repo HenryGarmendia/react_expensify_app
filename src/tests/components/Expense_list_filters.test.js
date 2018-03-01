35d1b55857da73ddcd689166de852cf23dabfe25
HenryGarmendia <me@henrygarmendia.com>
2018-02-28 19:41:12 -0600

    re-factor expense_list_filters.js to work with the test

---
+++ src/tests/components/Expense_list_filters.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { shallow } from 'enzyme';
+import { ExpenseListFilters } from '../../components/expense_list_filters';
+import { filters, altFilters } from '../fixtures/filters';
+
