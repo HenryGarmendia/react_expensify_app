0aaf4077a8f74c622f8d30cfbcdc6a2aee348d6d
HenryGarmendia <me@henrygarmendia.com>
2018-03-07 09:57:03 -0600

    added expense_summary.js and test to the project

--- src/components/expense_dashboard.js
+++ src/components/expense_dashboard.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ExpenseList from './expense_list';
 import ExpenseListFilters from './expense_list_filters';
+import ExpenseSummary from './expense_summary';
 
 const ExpenseDashboard = () => (
     <div>
+        <ExpenseSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
