ff4a33681639624b4c0724181f2cba222f010754
HenryGarmendia <me@henrygarmendia.com>
2018-03-08 19:25:11 -0600

    modified the firebase environment

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 const config = {
-    apiKey: "AIzaSyDz6ayhBlZphOD5xtCT8fn1bvfPWkjAlFw",
-    authDomain: "expensify-web-app.firebaseapp.com",
-    databaseURL: "https://expensify-web-app.firebaseio.com",
-    projectId: "expensify-web-app",
-    storageBucket: "expensify-web-app.appspot.com",
-    messagingSenderId: "1060104096199"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
