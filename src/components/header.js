a118ef74b7d4523b19fbfb03fbadbbc1134facc0
HenryGarmendia <me@henrygarmendia.com>
2018-03-14 10:52:01 -0500

    style the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-    <header>    
-        <h1>Expensify Web-App</h1>
-        <NavLink activeClassName="is_active link" to="/dashboard">Dashboard</NavLink>
-        <NavLink activeClassName="is_active link" to="/create">Create Expense</NavLink>
-        <button onClick={startLogout}>Logout</button>
+    <header className="header">
+        <div className="content-container">
+            <div className="header__content">
+                <Link className="header__title" to="/dashboard">
+                    <h1>Expensify App 💰</h1>
+                </Link>
+                <button onClick={startLogout}>Logout</button>
+            </div>
+        </div>    
     </header>
 );
 
