82a09887f13a2cc512270458b32242c8f28ba86d
HenryGarmendia <me@henrygarmendia.com>
2018-03-16 08:44:09 -0500

    change the expense_dashboard name and is test

--- src/tests/components/ExpenseDashboardPage.test.js
+++ src/tests/components/ExpenseDashboardPage.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ExpenseDashboardPage from '../../components/expense_dashboard';
+import ExpenseDashboard from '../../components/ExpenseDashboard';
 
-test('should render ExpenseDashboardPage correctly', () => {
-  const wrapper = shallow(<ExpenseDashboardPage />);
+test('should render ExpenseDashboard correctly', () => {
+  const wrapper = shallow(<ExpenseDashboard />);
   expect(wrapper).toMatchSnapshot();
 });
