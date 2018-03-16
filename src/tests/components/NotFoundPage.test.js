a07fc7c2171deb2406bc2215098ed2986295049e
HenryGarmendia <me@henrygarmendia.com>
2018-03-15 19:02:23 -0500

    updated the name of login_page.test.js and login_page.test.js

--- src/tests/components/NotFoundPage.test.js
+++ src/tests/components/NotFoundPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import NotFoundPage from '../../components/not_found_page';
+import NotFoundPage from '../../components/NotFoundPage';
 
 test('should render NotFoundPage correctly', () => {
     const wrapper = shallow(<NotFoundPage />);
